421ce89198d76ff35fadac2f6b918e929054b567
chintan29012001 <chintan19239@iiitd.ac.in>
2020-09-26 11:27:42 +0530

    l

--- 1long/part2/cd.h
+++ 1long/part2/cd.h
@@ -1,11 +1,27 @@
 #include<stdio.h> 
 #include<unistd.h>
 #include<stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 //cd
 
 void cd()
 {
     chdir(getenv("HOME"));
     // printf("%s",getenv("HOME"));
-
+}
+void cd(char path[])
+{
+    if(path==NULL)
+        return;
+    struct stat s;
+    int err=stat(path,&s);
+    if(err==0&& S_ISDIR(s.st_mode))
+    {
+        printf("exists\n");
+    }
+    else 
+    {
+        printf("does not exit");
+    }
 }
