147c3076fd14c008054dc3e6b7d78efab0d317cf
chintan19239 <chintan19239@iiiitd.ac.in>
2020-12-01 10:27:49 +0530

    working code without queue struct

--- assig4/blocking_mutex.c
+++ assig4/blocking_mutex.c
@@ -4,50 +4,7 @@
 #include <unistd.h> 
 #include <stdlib.h>
 
-struct node
-{
-   // pthread_t thread_id;
-   int index;
-   struct node* next;
-};
-struct queue
-{
-   struct node* front;
-   struct node* rear;
-};
-void insert(struct queue* q,int i)
-{
-   struct node* n=(struct node*)malloc(sizeof(struct node));
-   n->index=i;
-   n->next=NULL;
-   if(q->front==NULL)
-   {
-      //printf("front is null\n");
-      q->rear=n;
-      q->front=q->rear;  
-   }
-   else
-   {
-      q->rear->next=n;
-      q->rear=q->rear->next;
-   }
-   //printf("Inserted node \n");
-   //printf("front %d\n",q->front->index);
-   
-}
-void delete(struct queue* q)
-{
-   if(q->front==NULL)
-      return;
-   else
-   {
-      q->front=q->front->next;
-      if(q->front==NULL)
-      {
-         q->rear=NULL;
-      }
-   } 
-}
+
 
 
 struct my_semaphore
@@ -56,9 +13,9 @@ struct my_semaphore
    int total_philosophers;
    pthread_mutex_t* forks;
    pthread_mutex_t* sauce;
-   struct queue* q;
+   // struct queue* q;
    pthread_mutex_t* vr_sem;
-   pthread_mutex_t* qu_sem;
+   // pthread_mutex_t* qu_sem;
    pthread_mutex_t* thread_sleep_locks; 
    pthread_cond_t* thread_sleep_locks_cond;
 
@@ -71,19 +28,17 @@ void initialize(int n)
    ar.total_philosophers=n;
    ar.entry_allowed=n;
    ar.vr_sem=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
-   ar.qu_sem=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
    ar.forks=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*n);
    ar.thread_sleep_locks=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*n);
-   ar.thread_sleep_locks_cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t)*n);
+   ar.thread_sleep_locks_cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
    ar.sauce=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*2);
-   ar.q=(struct queue*)malloc(sizeof(struct queue));
    //printf("memory allocated \n");
    for(int i=0;i<n;i++)
    {
       pthread_mutex_init(&ar.forks[i],NULL);
    }
    pthread_mutex_init(ar.vr_sem,NULL);
-   pthread_mutex_init(ar.qu_sem,NULL);
+   pthread_cond_init(ar.thread_sleep_locks_cond,NULL);
    //printf("ini complete \n");
    
 }
@@ -92,20 +47,18 @@ void wait(int index)
 {
    //printf("in wait %d \n",index);
    pthread_mutex_lock(ar.vr_sem);
-   ar.entry_allowed-=1;
+   ar.entry_allowed-=2;
    // pthread_mutex_unlock(ar.vr_sem);
    if(ar.entry_allowed<0)
    {
       pthread_mutex_unlock(ar.vr_sem);
       
-      //printf("entry was not allowed \n");
-      pthread_mutex_lock(ar.qu_sem);
-      insert(ar.q,index);
-      pthread_mutex_unlock(ar.qu_sem);
+      // printf("entry was not allowed \n");
+      
       pthread_mutex_lock(&ar.thread_sleep_locks[index]);
       //printf("locked and waiting \n");
       
-      pthread_cond_wait(&ar.thread_sleep_locks_cond[index],&ar.thread_sleep_locks[index]);
+      pthread_cond_wait(ar.thread_sleep_locks_cond,&ar.thread_sleep_locks[index]);
       //printf("woken up\n");
       pthread_mutex_unlock(&ar.thread_sleep_locks[index]);
    }
@@ -135,7 +88,7 @@ void signal(int index)
 {
    //printf("in signal %d \n",index);
    pthread_mutex_lock(ar.vr_sem);
-   ar.entry_allowed+=1;
+   ar.entry_allowed+=2;
    pthread_mutex_unlock(ar.vr_sem);
    pthread_mutex_unlock(&ar.thread_sleep_locks[index]);
    pthread_mutex_unlock(&ar.thread_sleep_locks[(index+1)%ar.total_philosophers]);
@@ -144,28 +97,27 @@ void signal(int index)
    pthread_mutex_lock(ar.vr_sem);
    if(ar.entry_allowed<=0)
    {
+      
       pthread_mutex_unlock(ar.vr_sem);
       //printf("released sem  \n");
-      pthread_mutex_lock(ar.qu_sem);
-      struct node* n=ar.q->front;
-      pthread_mutex_unlock(ar.qu_sem);
+      // pthread_mutex_lock(ar.qu_sem);
+      // struct node* n=ar.q->front;
+      // pthread_mutex_unlock(ar.qu_sem);
       //printf("n %ld \n",&n);
-      if(n!=NULL)
-      {
+
+      pthread_cond_signal(ar.thread_sleep_locks_cond);
          // //printf("waking up %d \n",n->index);
          //printf("waking up %d \n",n->index);
-         pthread_mutex_lock(&ar.thread_sleep_locks[n->index]);
+         // pthread_mutex_lock(&ar.thread_sleep_locks[n->index]);
          //printf("acquired the lock %d \n",n->index);
          // pthread_mutex_unlock(ar.sem);
-         pthread_cond_signal(&ar.thread_sleep_locks_cond[n->index]);
-         pthread_mutex_unlock(&ar.thread_sleep_locks[n->index]);
+         
+         // pthread_mutex_unlock(&ar.thread_sleep_locks[n->index]);
          // sleep(1);
-         pthread_mutex_lock(ar.qu_sem);
-         delete(ar.q);  
-         pthread_mutex_unlock(ar.qu_sem);
-      }
+         
       
-      free(n);
+      
+      // free(n);
    }
    else
       pthread_mutex_unlock(ar.vr_sem);
@@ -178,12 +130,13 @@ void *philosopher(int i)
    
       while(1)
     {
+       sleep(1);//thinking
       wait(i);
       // pthread_mutex_lock(ar.vr_sem);
-      printf("Philosopher %lld eats using forks %d and %d \n",pthread_self(),i,(i+1)%ar.total_philosophers);
-      sleep(1);
+      printf("Philosopher %ld eats using forks %d and %d \n",pthread_self(),i,(i+1)%ar.total_philosophers);
+      sleep(1);//eating
       signal(i);
-      sleep(1);
+      // sleep(1);
     } 
    
 
@@ -202,7 +155,7 @@ int main()
       for(int i=0;i<n;i++)
       {
          pthread_create(&ar1[i],NULL,philosopher,i);
-         // sleep(1);
+         sleep(1);
       }
       for(int i=0;i<n;i++)
       {
@@ -216,11 +169,9 @@ int main()
          // pthread_mutex_destroy(&ar.thread_sleep_locks_cond[i]);
       }
       pthread_mutex_destroy(ar.vr_sem);
-      pthread_mutex_destroy(ar.qu_sem);
       pthread_mutex_destroy(&ar.sauce[0]);
       pthread_mutex_destroy(&ar.sauce[1]);
-      free(ar1);
-      free(ar.q);
+      
       sleep(1);
    exit(0);
 }
