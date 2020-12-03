5cce6a7298cda13fb4a3b58859be4404636cbd7a
chintan19239 <chintan19239@iiiitd.ac.in>
2020-12-03 22:36:42 +0530

    afdas

--- assig4/blocking_mutex.c
+++ assig4/blocking_mutex.c
@@ -9,129 +9,150 @@
 
 struct my_semaphore
 {
-   int entry_allowed;
-   int total_philosophers;
-   pthread_mutex_t* forks;
-   pthread_mutex_t* sauce;
-   // struct queue* q;
-   pthread_mutex_t* vr_sem;
-   // pthread_mutex_t* qu_sem;
-   pthread_mutex_t* thread_sleep_locks; 
-   pthread_cond_t* thread_sleep_locks_cond;
+   int entry;
+   pthread_mutex_t* lock;
+   pthread_cond_t* cond;
+};
 
-}ar;
-
+struct my_semaphore* forks;
+struct my_semaphore* sauce;
+int total_philosophers;
 
 void initialize(int n)
 {
    //printf("ini started \n");
-   ar.total_philosophers=n;
-   ar.entry_allowed=1;
-   ar.vr_sem=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
-   ar.forks=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*n);
-   ar.thread_sleep_locks=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*n);
-   ar.thread_sleep_locks_cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
-   ar.sauce=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*2);
-   //printf("memory allocated \n");
+   total_philosophers=n;
+   forks=(struct my_semaphore*)malloc(sizeof(struct my_semaphore)*n);
    for(int i=0;i<n;i++)
    {
-      pthread_mutex_init(&ar.forks[i],NULL);
+      forks[i].entry=1;
+      forks[i].lock=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+      forks[i].cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+      pthread_mutex_init(forks[i].lock,NULL);
+      pthread_cond_init(forks[i].cond,NULL);
    }
-   pthread_mutex_init(ar.vr_sem,NULL);
-   pthread_cond_init(ar.thread_sleep_locks_cond,NULL);
+   sauce=(struct my_semaphore*)malloc(sizeof(struct my_semaphore));
+   sauce->entry=1;
+   sauce->lock=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+   sauce->cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+   pthread_mutex_init(sauce->lock,NULL);
+   pthread_cond_init(sauce->cond,NULL);  
    //printf("ini complete \n");
    
 }
 //first n-1 philosophers pich their chopsticks and then they try to to get sauces whoever gets the first eats and the releases it
+void take_fork(int index)
+{
+      pthread_mutex_lock(forks[index].lock);
+      forks[index].entry-=1;
+      if(forks[index].entry<0)
+      {
+         pthread_mutex_unlock(forks[index].lock);
+         // printf("locked and waiting %d \n",ar.entry_allowed);
+         pthread_mutex_t temp;
+         pthread_mutex_init(&temp,NULL);
+         pthread_mutex_lock(&temp);
+         pthread_cond_wait(forks[index].cond,&temp);
+         pthread_mutex_unlock(&temp);
+         // printf("woken up\n");
+         
+      }
+      else
+         pthread_mutex_unlock(forks[index].lock);
+
+}
+void take_sauces()
+{
+      pthread_mutex_lock(sauce->lock);
+      sauce->entry-=1;
+      if(sauce->entry<0)
+      {
+         pthread_mutex_unlock(sauce->lock);
+         // printf("locked and waiting %d \n",ar.entry_allowed);
+         pthread_mutex_t temp;
+         pthread_mutex_init(&temp,NULL);
+         pthread_mutex_lock(&temp);
+         pthread_cond_wait(sauce->cond,&temp);
+         pthread_mutex_unlock(&temp);
+         // printf("woken up\n");
+         
+      }
+      else
+         pthread_mutex_unlock(sauce->lock);
+
+}
+
 void wait(int index)
 {
    //printf("in wait %d \n",index);
-   pthread_mutex_lock(ar.vr_sem);
-   ar.entry_allowed-=1;
-   pthread_mutex_unlock(ar.vr_sem);
-   pthread_mutex_lock(ar.vr_sem);
-   if(ar.entry_allowed<0)
-   {
-      pthread_mutex_unlock(ar.vr_sem);
-      
-      // printf("entry was not allowed \n");
-      
-      pthread_mutex_lock(&ar.thread_sleep_locks[index]);
-      // printf("locked and waiting %d \n",ar.entry_allowed);
-      
-      pthread_cond_wait(ar.thread_sleep_locks_cond,&ar.thread_sleep_locks[index]);
-      // printf("woken up\n");
-      pthread_mutex_unlock(&ar.thread_sleep_locks[index]);
-   }
-   else
-      pthread_mutex_unlock(ar.vr_sem);
+   int sec=(index+1)%total_philosophers;
    if(index%2==0)
    {
-      pthread_mutex_lock(&ar.forks[index]);
-      pthread_mutex_lock(&ar.forks[(index+1)%ar.total_philosophers]);
-      pthread_mutex_lock(&ar.sauce[0]);
-      pthread_mutex_lock(&ar.sauce[1]); 
+      take_sauces();
+      take_fork(index);
+      take_fork(sec);  
+      
 
    }
    else
    {
-      pthread_mutex_lock(&ar.forks[(index+1)%ar.total_philosophers]);
-      pthread_mutex_lock(&ar.forks[index]);
-      pthread_mutex_lock(&ar.sauce[0]);
-      pthread_mutex_lock(&ar.sauce[1]); 
+      take_sauces();
+      take_fork(sec);
+      take_fork(index);
+       
 
    }
    
-   
    //printf("out of wait %d \n",index);
+}
+void release_fork(int index)
+{
+   pthread_mutex_lock(forks[index].lock);
+   forks[index].entry+=1;
+   if(forks[index].entry<=0)
+      pthread_cond_signal(forks[index].cond);
+   pthread_mutex_unlock(forks[index].lock);
+}
+void release_sauce()
+{
+   pthread_mutex_lock(sauce->lock);
+   sauce->entry+=1;
+   if(sauce->entry<=0)
+      pthread_cond_signal(sauce->cond);
+   pthread_mutex_unlock(sauce->lock);
+
 }
 void signal(int index)
 {
    //printf("in signal %d \n",index);
-   pthread_mutex_lock(ar.vr_sem);
-   ar.entry_allowed+=1;
-   pthread_mutex_unlock(ar.vr_sem);
-   pthread_mutex_unlock(&ar.forks[index]);
-   pthread_mutex_unlock(&ar.forks[(index+1)%ar.total_philosophers]);
-   pthread_mutex_unlock(&ar.sauce[0]);
-   pthread_mutex_unlock(&ar.sauce[1]); 
-   pthread_mutex_lock(ar.vr_sem);
-   if(ar.entry_allowed<=0)
-   {
-      
-      pthread_mutex_unlock(ar.vr_sem);
-      //printf("released sem  \n");
-      // pthread_mutex_lock(ar.qu_sem);
-      // struct node* n=ar.q->front;
-      // pthread_mutex_unlock(ar.qu_sem);
-      //printf("n %ld \n",&n);
+   int sec=(index+1)%total_philosophers;
+   release_fork(index);
+   release_fork(sec);
+   release_sauce();
+}
 
-      pthread_cond_signal(ar.thread_sleep_locks_cond);
-         // //printf("waking up %d \n",n->index);
-         //printf("waking up %d \n",n->index);
-         // pthread_mutex_lock(&ar.thread_sleep_locks[n->index]);
-         //printf("acquired the lock %d \n",n->index);
-         // pthread_mutex_unlock(ar.sem);
-         
-         // pthread_mutex_unlock(&ar.thread_sleep_locks[n->index]);
-         // sleep(1);
-         
-      
-      
-      // free(n);
-   }
-   else
-      pthread_mutex_unlock(ar.vr_sem);
-   //printf("out of signal %d \n",index);
-   
+void print_val_fork(int index)
+{
+   pthread_mutex_lock(forks[index].lock);
+   printf("fork %d  val %d\n",index,forks[index].entry);
+   pthread_mutex_unlock(forks[index].lock);
 
 }
-void signal_printval()
+void print_val_sauce()
 {
-   pthread_mutex_lock(ar.vr_sem);
-   printf("%d\n",ar.entry_allowed);
-   pthread_mutex_unlock(ar.vr_sem);
+   pthread_mutex_lock(sauce->lock);
+   printf("sauce val %d\n",sauce->entry);
+   pthread_mutex_unlock(sauce->lock);
 
+}
+
+
+void signal_printval(int index)
+{
+   print_val_fork(index);
+   int sec=(index+1)%total_philosophers;
+   print_val_fork(sec);
+   print_val_sauce();
 }
 void *philosopher(int i)
 {
@@ -141,9 +162,11 @@ void *philosopher(int i)
       //  sleep(2);//thinking
       // sleep(1);
       // printf("start\n");
+      
       wait(i);
       // pthread_mutex_lock(ar.vr_sem);
-      printf("Philosopher %ld eats using forks %d and %d  \n",pthread_self(),i,(i+1)%ar.total_philosophers);
+      printf("Philosopher %ld eats using forks %d and %d  \n",pthread_self(),i,(i+1)%total_philosophers);
+      // signal_printval(i);
       // signal_printval();
       // pthread_mutex_unlock(ar.vr_sem);
       sleep(1);
@@ -159,32 +182,20 @@ int main()
  
    int n=0;
    printf("enter the no of philosophers \n");
-   scanf("%d",&n);
+   scanf("%d",&n);   
+   initialize(n);
+   pthread_t* ar1=(pthread_t*)malloc(sizeof(pthread_t)*n);
+   for(int i=0;i<n;i++)
+   {
+      pthread_create(&ar1[i],NULL,philosopher,i);
+      // sleep(1);
+   }
+   for(int i=0;i<n;i++)
+   {
+      pthread_join(ar1[i],NULL);
+
+   }
    
-
-      
-      initialize(n);
-      pthread_t* ar1=(pthread_t*)malloc(sizeof(pthread_t)*n);
-      for(int i=0;i<n;i++)
-      {
-         pthread_create(&ar1[i],NULL,philosopher,i);
-         sleep(1);
-      }
-      for(int i=0;i<n;i++)
-      {
-         pthread_join(ar1[i],NULL);
-
-      }
-      for(int i=0;i<n;i++)
-      {
-         pthread_mutex_destroy(&ar.thread_sleep_locks[i]);
-         pthread_mutex_destroy(&ar.forks[i]);
-         // pthread_mutex_destroy(&ar.thread_sleep_locks_cond[i]);
-      }
-      pthread_mutex_destroy(ar.vr_sem);
-      pthread_mutex_destroy(&ar.sauce[0]);
-      pthread_mutex_destroy(&ar.sauce[1]);
-      free(ar1);
       
       sleep(1);
    exit(0);
--- assig4/non_blocking_mutex.c
+++ assig4/non_blocking_mutex.c
@@ -1,3 +1,4 @@
+
 #include <pthread.h> 
 #include <stdio.h> 
 #include <unistd.h> 
@@ -5,120 +6,242 @@
 
 
 
+
 struct my_semaphore
 {
-   int entry_allowed;
-   int total_philosophers;
-   pthread_mutex_t* forks;
-   pthread_mutex_t* sauce;
-   // struct queue* q;
-   pthread_mutex_t* vr_sem;
-   // pthread_mutex_t* qu_sem;
-   pthread_mutex_t* thread_sleep_locks; 
-   pthread_cond_t* thread_sleep_locks_cond;
+   int entry;
+   pthread_mutex_t* lock;
+   pthread_cond_t* cond;
+};
 
-}ar;
-
+struct my_semaphore* forks;
+struct my_semaphore* sauce;
+int total_philosophers;
 
 void initialize(int n)
 {
    //printf("ini started \n");
-   ar.total_philosophers=n;
-   ar.entry_allowed=1;
-   ar.vr_sem=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
-   ar.forks=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*n);
-   ar.thread_sleep_locks=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*n);
-   ar.thread_sleep_locks_cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
-   ar.sauce=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*2);
-   
-   //printf("memory allocated \n");
+   total_philosophers=n;
+   forks=(struct my_semaphore*)malloc(sizeof(struct my_semaphore)*n);
    for(int i=0;i<n;i++)
    {
-      pthread_mutex_init(&ar.forks[i],NULL);
+      forks[i].entry=1;
+      forks[i].lock=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+      forks[i].cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+      pthread_mutex_init(forks[i].lock,NULL);
+      pthread_cond_init(forks[i].cond,NULL);
    }
-   pthread_mutex_init(ar.vr_sem,NULL);
-   pthread_cond_init(ar.thread_sleep_locks_cond,NULL);
+   sauce=(struct my_semaphore*)malloc(sizeof(struct my_semaphore));
+   sauce->entry=1;
+   sauce->lock=(pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+   sauce->cond=(pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+   pthread_mutex_init(sauce->lock,NULL);
+   pthread_cond_init(sauce->cond,NULL);  
    //printf("ini complete \n");
    
 }
 //first n-1 philosophers pich their chopsticks and then they try to to get sauces whoever gets the first eats and the releases it
+int take_fork(int index)
+{
+      if(pthread_mutex_trylock(forks[index].lock)!=0)
+         return 1;
+      forks[index].entry-=1;
+      if(forks[index].entry<0)
+      {
+         forks[index].entry+=1;
+         pthread_mutex_unlock(forks[index].lock);
+         // printf("locked and waiting %d \n",ar.entry_allowed);
+         return 1;
+         // printf("woken up\n");
+         
+      }
+      else
+         pthread_mutex_unlock(forks[index].lock);
+      return 0;
+
+}
+int take_sauces()
+{
+      if(pthread_mutex_trylock(sauce->lock)!=0)
+         return 1;
+      sauce->entry-=1;
+      if(sauce->entry<0)
+      {
+         sauce->entry+=1;
+         pthread_mutex_unlock(sauce->lock);
+         // printf("locked and waiting %d \n",ar.entry_allowed);
+         return 1;
+         // printf("woken up\n");
+         
+      }
+      else
+         pthread_mutex_unlock(sauce->lock);
+      return 0;
+}
+
 int wait(int index)
 {
    //printf("in wait %d \n",index);
-   int x=0;
-   x+=pthread_mutex_trylock(ar.vr_sem);
-   ar.entry_allowed-=1;
-   // pthread_mutex_unlock(ar.vr_sem);
-   if(ar.entry_allowed<0)
-   {
-      x+=pthread_mutex_unlock(ar.vr_sem);
-      
-      //printf("entry was not allowed \n");
-      return 1;
-   }
-   else
-      x+=pthread_mutex_unlock(ar.vr_sem);
+   int sec=(index+1)%total_philosophers;
+   if(take_sauces()!=0)
+         return 1;
    if(index%2==0)
    {
-      x+=pthread_mutex_trylock(&ar.thread_sleep_locks[index]);
-      x+=pthread_mutex_trylock(&ar.thread_sleep_locks[(index+1)%ar.total_philosophers]);
-      x+=pthread_mutex_trylock(&ar.sauce[0]);
-      x+=pthread_mutex_trylock(&ar.sauce[1]); 
+      
+      if(take_fork(index)!=0)
+         return 1;
+      if(take_fork(sec)!=0)
+      {
+         release_fork(index);
+         return 2;  
+
+      }
 
    }
    else
    {
-      x+=pthread_mutex_trylock(&ar.thread_sleep_locks[(index+1)%ar.total_philosophers]);
-      x+=pthread_mutex_trylock(&ar.thread_sleep_locks[index]);
-      x+=pthread_mutex_trylock(&ar.sauce[0]);
-      x+=pthread_mutex_trylock(&ar.sauce[1]); 
+      if(take_fork(sec)!=0)
+         return 1;
+      if(take_fork(index)!=0)
+      {
+         release_fork(sec);
+         return 3;
+      }
+       
 
    }
-   return x;
+   return 0;
    
    //printf("out of wait %d \n",index);
 }
+int release_fork(int index)
+{
+   pthread_mutex_lock(forks[index].lock);
+   forks[index].entry+=1;
+   pthread_mutex_unlock(forks[index].lock);
+   
+}
+void release_sauce()
+{
+   pthread_mutex_lock(sauce->lock);
+   sauce->entry+=1;
+   pthread_mutex_unlock(sauce->lock);
+
+}
+int release_fork_non_block(int index)
+{
+   if(pthread_mutex_trylock(forks[index].lock)!=0)
+      return 1;
+   forks[index].entry+=1;
+   pthread_mutex_unlock(forks[index].lock);
+
+}
+
+int release_sauce_non_block()
+{
+   if(pthread_mutex_lock(sauce->lock)!=0)
+      return 1;
+   sauce->entry+=1;
+   pthread_mutex_unlock(sauce->lock);
+
+}
+
 int signal(int index)
 {
    //printf("in signal %d \n",index);
-   int x=0;
-   x+=pthread_mutex_trylock(ar.vr_sem);
-   ar.entry_allowed+=1;
-   x+=pthread_mutex_unlock(ar.vr_sem);
-   x+=pthread_mutex_unlock(&ar.thread_sleep_locks[index]);
-   x+=pthread_mutex_unlock(&ar.thread_sleep_locks[(index+1)%ar.total_philosophers]);
-   x+=pthread_mutex_unlock(&ar.sauce[0]);
-   x+=pthread_mutex_unlock(&ar.sauce[1]); 
-   //printf("out of signal %d \n",index);
-   return x;
-   
+   int sec=(index+1)%total_philosophers;
+   if(release_fork_non_block(index)!=0)
+      return 1;
+   if(release_fork_non_block(sec)!=0)
+      return 2;
+   if(release_sauce_non_block()!=0)
+      return 3;
+   return 0;
+}
+void print_val_fork(int index)
+{
+   pthread_mutex_lock(forks[index].lock);
+   printf("fork %d  val %d\n",index,forks[index].entry);
+   pthread_mutex_unlock(forks[index].lock);
 
+}
+void print_val_sauce()
+{
+   pthread_mutex_lock(sauce->lock);
+   printf("sauce val %d\n",sauce->entry);
+   pthread_mutex_unlock(sauce->lock);
+
+}
+
+
+void signal_printval(int index)
+{
+   print_val_fork(index);
+   int sec=(index+1)%total_philosophers;
+   print_val_fork(sec);
+   print_val_sauce();
+}
+
+void print_val_fork_non_blocking(int index)
+{
+   if(pthread_mutex_trylock(forks[index].lock)!=0)
+      return;
+   printf("fork %d  val %d\n",index,forks[index].entry);
+   pthread_mutex_unlock(forks[index].lock);
+
+}
+void print_val_sauce_non_blocking()
+{
+   if(pthread_mutex_trylock(sauce->lock)!=0)
+      return;
+   printf("sauce val %d\n",sauce->entry);
+   pthread_mutex_unlock(sauce->lock);
+
+}
+
+
+int signal_printval_non_blocking(int index)
+{
+   print_val_fork_non_blocking(index);
+   int sec=(index+1)%total_philosophers;
+   print_val_fork_non_blocking(sec);
+   print_val_sauce_non_blocking();
 }
 void *philosopher(int i)
 {
    
-    //   sleep(1);//thinking
-    while(1)
+      while(1)
     {
-       sleep(1);//thinking
-      if(wait(i)!=0)
-        {
-            // signal(i);
-            pthread_mutex_trylock(ar.vr_sem);
-            ar.entry_allowed+=1;
-            pthread_mutex_unlock(ar.vr_sem);
-            pthread_mutex_unlock(&ar.thread_sleep_locks[i]);
-            pthread_mutex_unlock(&ar.thread_sleep_locks[(i+1)%ar.total_philosophers]);
-            pthread_mutex_unlock(&ar.sauce[0]);
-            pthread_mutex_unlock(&ar.sauce[1]);
-            // printf("not got all %d\n",i);
-            continue;
-        }
-      // x+=pthread_mutex_trylock(ar.vr_sem);
-      printf("Philosopher %ld eats using forks %d and %d \n",pthread_self(),i,(i+1)%ar.total_philosophers);
+      //  sleep(2);//thinking
+      // sleep(1);
+      // printf("start\n");
+      int x=wait(i);
+      switch (x)
+      {
+      case 2:release_fork(i);
+         break;
+      case 3:release_fork((i+1)%total_philosophers);
+         break;
+      }
+      if(x!=0)
+         continue;
+      // pthread_mutex_lock(ar.vr_sem);
+      printf("Philosopher %ld eats using forks %d and %d  \n",pthread_self(),i,(i+1)%total_philosophers);
+      // signal_printval(i);
+      // signal_printval_non_blocking(i);
+      // signal_printval();
+      // pthread_mutex_unlock(ar.vr_sem);
       sleep(1);
-      signal(i);
+      int y=signal(i);
       
+      switch(y)
+      {
+         case 3:release_sauce();
+         case 2:release_fork((i+1)%total_philosophers);
+         case 1:release_fork(i);break;
+      }
+      sleep(1);
+      // printf("exit\n");
     } 
    
 
@@ -128,31 +251,22 @@ int main()
  
    int n=0;
    printf("enter the no of philosophers \n");
-   scanf("%d",&n);
-    initialize(n);
-    pthread_t* ar1=(pthread_t*)malloc(sizeof(pthread_t)*n);
-    for(int i=0;i<n;i++)
-    {
-        pthread_create(&ar1[i],NULL,philosopher,i);
-        sleep(1);
-    }
-    for(int i=0;i<n;i++)
-    {
-        pthread_join(ar1[i],NULL);
+   scanf("%d",&n);   
+   initialize(n);
+   pthread_t* ar1=(pthread_t*)malloc(sizeof(pthread_t)*n);
+   for(int i=0;i<n;i++)
+   {
+      pthread_create(&ar1[i],NULL,philosopher,i);
+      // sleep(1);
+   }
+   for(int i=0;i<n;i++)
+   {
+      pthread_join(ar1[i],NULL);
 
-    }
-    for(int i=0;i<n;i++)
-    {
-        pthread_mutex_destroy(&ar.thread_sleep_locks[i]);
-        pthread_mutex_destroy(&ar.forks[i]);
-        // pthread_mutex_destroy(&ar.thread_sleep_locks_cond[i]);
-    }
-    pthread_mutex_destroy(ar.vr_sem);
-    
-    pthread_mutex_destroy(&ar.sauce[0]);
-    pthread_mutex_destroy(&ar.sauce[1]);
-    free(ar1);
-    sleep(1);
-
+   }
+   
+      
+      sleep(1);
    exit(0);
 }
+
